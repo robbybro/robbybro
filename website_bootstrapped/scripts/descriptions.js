e2efc951599b0bca1c899a017fcc01a5d396bdaf
Robby Brosman <robbybro@uw.edu>
2013-10-31 21:43:46 -0700

    bootstrapped site, currently adding portfolio information and functionality. Need to upload to get online reference to images so my render() function can read the image url's.... I really should download Xampp...

---
+++ website_bootstrapped/scripts/descriptions.js
@@ -0,0 +1,17 @@
+// design
+
+var HRC_DESIGN_DESC = 'I won the 2013-14 Husky Running Club Shirt design contest with this submission. The design was printed on technical running shirts and sold to club members.';
+
+var ROBBYBRO_DESC = 'I designed a logo for myself in high school (top left) and wanted to iterate over it and create something truly classy and memorable. This is what I ended up with.';
+
+var PURPLE_SQUIRREL_DESC = 'I designed this logo for a start-up that never ended up taking off.';
+
+// develop
+
+var HRC_DEVELOP_DESC;
+
+var REFLEX_DESC;
+
+var CLEARWIRE_DESC;
+
+var ARDUINO_DESC;
