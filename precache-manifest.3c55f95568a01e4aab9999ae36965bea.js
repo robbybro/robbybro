f2e33690a0f95c04a03f579864afbc0da995d018
Robby Brosman <robbybro@fb.com>
2019-03-08 20:31:22 -0800

    Updates

---
+++ precache-manifest.3c55f95568a01e4aab9999ae36965bea.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "6aca757549bd996f2d3907436a51ba86",
+    "url": "/static/media/rb_logo.6aca7575.svg"
+  },
+  {
+    "revision": "4dc2d9b07f6a30545283",
+    "url": "/static/css/main.9c617cdf.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "122fc48900a5abf39d0f2916d1163151",
+    "url": "/static/media/spoon_green.122fc489.png"
+  },
+  {
+    "revision": "09387e881cff67bdaba1",
+    "url": "/static/js/2.09387e88.chunk.js"
+  },
+  {
+    "revision": "5b55e0b9b07d29d050a9cb15bf022763",
+    "url": "/static/media/me.5b55e0b9.jpg"
+  },
+  {
+    "revision": "c61bd12cb7896b15060fabc404f1760d",
+    "url": "/static/media/abaloneyoga.c61bd12c.png"
+  },
+  {
+    "revision": "a57bde920b871ae5494980e976abff10",
+    "url": "/static/media/insta.a57bde92.png"
+  },
+  {
+    "revision": "b1473eb468bb01767ff897f9b2ca37f5",
+    "url": "/static/media/gcal.b1473eb4.png"
+  },
+  {
+    "revision": "41b6413500a2d9cd7432dcd6e257fc55",
+    "url": "/static/media/flownetwork.41b64135.jpg"
+  },
+  {
+    "revision": "4dc2d9b07f6a30545283",
+    "url": "/static/js/main.4dc2d9b0.chunk.js"
+  },
+  {
+    "revision": "18fbb027bbf5ae3615d86de8822d93b5",
+    "url": "/static/media/spoon_blue.18fbb027.png"
+  },
+  {
+    "revision": "d4a173008f776a871c4e2fe093e4c8a3",
+    "url": "/static/media/spoon_lightblue.d4a17300.png"
+  },
+  {
+    "revision": "b02d5363697a2eeeef5a8e7e56e20d0b",
+    "url": "/static/media/spoon_magenta.b02d5363.png"
+  },
+  {
+    "revision": "5482cec197b87286d3a0241705f816ad",
+    "url": "/static/media/spoon_orange.5482cec1.png"
+  },
+  {
+    "revision": "2f1da6fc8c091b473f52ed43b58833ca",
+    "url": "/static/media/spoon_purple.2f1da6fc.png"
+  },
+  {
+    "revision": "b49ff243a04b88dccf35a2ffde1d13ac",
+    "url": "/static/media/spoon_red.b49ff243.png"
+  },
+  {
+    "revision": "7df0612218cfbaa701ff3bd176efa9bd",
+    "url": "/static/media/spoon_yellow.7df06122.png"
+  },
+  {
+    "revision": "1835c64b174e7dc2d7c3f31c66fb77de",
+    "url": "/static/media/spoon_lightgreen.1835c64b.png"
+  },
+  {
+    "revision": "09387e881cff67bdaba1",
+    "url": "/static/css/2.04882b9b.chunk.css"
+  },
+  {
+    "revision": "8a4dea152a73a4e03448846cd5394a5d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b360911e8350b1ee510d712fa785b1b.js"
+  "/precache-manifest.3c55f95568a01e4aab9999ae36965bea.js"
 );
 
 workbox.clientsClaim();
